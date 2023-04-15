f26b31acd5149b9202a40134f6712c092cf1d68b
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-04-15 17:45:27 +0200

    added basic setup and design login page

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvgqKBmrITnVwe6gjhewfR0YTPwvAO2Fo",
+  authDomain: "auth-firebase-context-ta-91aa7.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-91aa7",
+  storageBucket: "auth-firebase-context-ta-91aa7.appspot.com",
+  messagingSenderId: "248382749045",
+  appId: "1:248382749045:web:404f10df61efed7395fb3c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
