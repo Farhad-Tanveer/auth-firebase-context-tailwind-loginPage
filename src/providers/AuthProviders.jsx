5dc86f61cbbafce81404fe3dc50b835546fbc9d4
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-04-16 11:49:49 +0200

    implemented log in registration

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 import { useState } from "react";
 
@@ -13,9 +17,14 @@ const AuthProviders = ({ children }) => {
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   const authInfo = {
     user,
     createUser,
+    signIn,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
